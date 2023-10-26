d95e179487f080b835ca54145943ac3f91d25c96
bjoernbuth <bjoernbuth76@gmail.com>
2023-10-26 20:59:50 +0200

    insert path only 1 above

--- source/conf.py
+++ source/conf.py
@@ -5,34 +5,34 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'supercalc'
-copyright = '2023, Bjoern'
-author = 'Bjoern'
-release = '1.0.0'
+project = "supercalc"
+copyright = "2023, Bjoern"
+author = "Bjoern"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.napoleon',
+    "sphinx.ext.napoleon",
 ]
 
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False  # set to True if you're also using Numpy-style
 napoleon_include_private_with_doc = False
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
